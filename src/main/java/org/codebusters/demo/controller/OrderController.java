753d3c3d0a16723e30ad5b0ee328b24f1108b653
Akshat Gupta <akshat2032003@gmail.com>
2026-04-18 11:36:25 +0530

    Added Order Controller

--- src/main/java/org/codebusters/demo/controller/OrderController.java
+++ src/main/java/org/codebusters/demo/controller/OrderController.java
@@ -5,16 +5,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/order")
 public class OrderController {
-    @GetMapping("/cart/{uid}")
+    @GetMapping("/{uid}")
     public String getCartItems(@PathVariable Long uid) {
         return "";
     }
 
-    @PostMapping("/add/{itmId}")
-    public String addItem(@PathVariable Long itmId, @RequestBody String path) {
+    @PostMapping("/add/")
+    public String addItem(@RequestParam Long itmId, @RequestBody String path) {
         return "";
     }
+
+    
 }
